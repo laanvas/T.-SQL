5de9aa9e277f9e378b5bc35d4e1586f6d68cd9e1
Alan Vasconcelos <145019770+alanvas@users.noreply.github.com>
2023-09-21 11:39:57 -0300

    21-09-23

---
+++ ex0025.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE PROCEDURE INCLUINDO_DADOS_VENDA 
+(
+p_ID produto_venda_exercicio.id%type,
+p_COD_PRODUTO produto_venda_exercicio.cod_produto%type,
+p_DATA produto_venda_exercicio.data%type,
+p_QUANTIDADE produto_venda_exercicio.quantidade%type,
+p_PRECO produto_venda_exercicio.preco%type,
+p_VALOR produto_venda_exercicio.valor_total%type,
+p_PERCENTUAL produto_venda_exercicio.percentual_imposto%type
+)
+IS
+BEGIN
+   INSERT INTO PRODUTO_VENDA_EXERCICIO 
+   (id, cod_produto, data, quantidade, preco, valor_total, percentual_imposto) 
+   VALUES 
+   (p_ID, p_COD_PRODUTO, p_DATA, p_QUANTIDADE, p_PRECO, p_VALOR, p_PERCENTUAL);
+    COMMIT;
+END;
---
+++ ex0027.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION RETORNA_IMPOSTO 
+(p_COD_PRODUTO produto_venda_exercicio.cod_produto%type)
+RETURN produto_venda_exercicio.percentual_imposto%type
+IS
+   v_CATEGORIA produto_exercicio.categoria%type;
+   v_IMPOSTO produto_venda_exercicio.percentual_imposto%type;
+BEGIN
+    v_CATEGORIA := retorna_categoria(p_COD_PRODUTO);
+    IF TRIM(v_CATEGORIA) = 'Sucos de Frutas' THEN
+        v_IMPOSTO := 10;
+    ELSIF  TRIM(v_CATEGORIA) = 'Águas' THEN
+        v_IMPOSTO := 20;
+    ELSIF  TRIM(v_CATEGORIA) = 'Mate' THEN
+        v_IMPOSTO := 15;
+    END IF;
+    RETURN v_IMPOSTO;
+END;
